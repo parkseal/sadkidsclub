f595f8be4fb3595ade3a191f50981c0ce0f409ab
parkseal <hupexapto@gmail.com>
2025-11-09 14:45:29 -0500

    google font bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>@import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap');
-</head>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <Script 
           src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"
